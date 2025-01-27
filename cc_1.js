77cf0fa46867b63f75bfeaa0fe13b3f6e72624dc
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-01-26 19:58:09 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,10 @@
+//Task 1: Employee Information
+let  employeeName = "Simon Francis";
+const employeeID = 99978;
+var isActive = true;
+
+console.log("Employee Name:", employeeName,"Type:", typeof employeeName);
+console.log("Employee ID:",employeeID,"Type:", typeof employeeID);
+console.log("Is Active:", isActive,"Type:", typeof isActive);
+
+//Task 2:Product Details
