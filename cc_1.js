64ef289cad98fba5401d5dff9896b91c523bbd46
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-01-26 20:10:06 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -8,3 +8,12 @@ console.log("Employee ID:",employeeID,"Type:", typeof employeeID);
 console.log("Is Active:", isActive,"Type:", typeof isActive);
 
 //Task 2:Product Details
+let productName = "PS5 Spider-Man 2"
+const productPrice = 49.99
+var isAvailable = false
+
+console.log("Product Name", productName, "Type:", typeof productName);
+console.log("Product Price", productPrice, "Type:",typeof productPrice);
+console.log("Is Available", isAvailable, "Type:", typeof isAvailable);
+
+//Task 3:Financial Transactions
